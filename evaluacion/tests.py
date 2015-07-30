8595f6e21767c315813f495cf045f4ac14ff5c60
Fabricio <fabricio@MacBook-Air-de-codetag.local>
2015-07-30 11:38:40 -0500

    adicion de test de unidad para la creacion de figuras

--- evaluacion/tests.py
+++ evaluacion/tests.py
@@ -15,3 +15,25 @@ class WorkSpaceTest(TestCase):
     def consultaEpacios(self):
         espacios = WorkSpace.objects.all()
         self.assertEqual(espacios[0].nombre, "espacio")
+
+class FiguraTest(TestCase):
+    def setUp(self):
+        WorkSpace.objects.create(nombre="espacio", limiteFiguras=5)
+
+    def crearCuadrado(self):
+        espacio = WorkSpace.objects.get(nombre="espacio")
+        Cuadrado.objects.create(lado=3, numLados=4, workspace=espacio)
+        cuadrado = Cuadrado.objects.get(workspace=espacio)
+        self.assertEqual(cuadrado.numLados, 4)
+
+    def crearTriangulo(self):
+        espacio = WorkSpace.objects.get(nombre="espacio")
+        Triangulo.objects.create(base=1,altura=1, hipotenusa=2, numLados=3, workspace=espacio)
+        triangulo = Triangulo.objects.get(workspace=espacio)
+        self.assertEqual(triangulo.numLados, 3)
+
+    def crearHexagono(self):
+        espacio = WorkSpace.objects.get(nombre="espacio")
+        Hexagono.objects.create(radio=4, numLados=6, workspace=espacio)
+        hexagono = Hexagono.objects.get(workspace=espacio)
+        self.assertEqual(hexagono.radio, 4)
